e093e9103225700f06ce18da5d6ed3ed45e74b74
김경현 <yuwongon@KHKIM-MIL.local>
2022-11-21 18:29:04 +0900

    [Setting] next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const withPlugins = require('next-compose-plugins');
+
+module.exports = async (phase) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'standalone', // for docker image
+    compress: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+      styledComponents: true,
+      // removeConsole: {
+      //   exclude: ['error'],
+      // },
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/apis/:path*',
+          destination: 'http://localhost:8081/:path*',
+        },
+      ];
+    },
+  };
+
+  return withPlugins([withBundleAnalyzer], nextConfig)(phase, { undefined });
+};
